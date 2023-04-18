61b6c3e12c04a22de66d28666d44d494a002bbbf
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2023-04-19 00:07:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,14 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+ });
+
+ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ ScrollReveal().reveal('.about-img, .home-content h1', { origin: 'left' });
+ ScrollReveal().reveal('.about-content, .home-content p', { origin: 'right' });
